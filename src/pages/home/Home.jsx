add9a7ae2c0c2607c1b287e13f15ac5bf5094d4a
Aisha Gregg <aishagregg@gmail.com>
2020-01-10 10:06:28 +0100

    style feature image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,54 +1,54 @@
 import React from "react";
 import styles from "./home.module.css";
-
-import { Link } from "../../components/link/Link";
+import { FeatureImage } from "../../components/feature-image/FeatureImage";
+import { useHistory } from "react-router-dom";
 
 export function Home() {
+  const history = useHistory();
+
   return (
     <main className={styles["wrapper-content"]}>
-      <img
-        src="./img/index-photo.jpg"
-        alt="profilepic"
-        className={styles["profile-photo"]}
-      />
-      <div>
-        <h3>About me</h3>
-        <p>
-          Hi. I am Aisha Kamilah; travel enthusiast and blogger. I am from the
-          beautiful twin isles of Trinidad and Tobago. Now I live in Spain and
-          travel frequently across Europe. For the past couple years and after
-          visiting over 30 cities my perspectives and visions of all that I have
-          known continues to be challenged. I want to somehow share my stories
-          with the world.
-        </p>
-        <p>
-          I am also a software developer. You can visit my programming blog
-          here.
-        </p>
+      <section className={styles.introduction}>
+        <img
+          src="./img/index-photo.jpg"
+          alt="profilepic"
+          className={styles["profile-photo"]}
+        />
         <div>
-          <h1>Recent Posts</h1>
-          <menu className={styles.container}>
-            <Link to="/travel/morocco">
-              <div>
-                <img src="./img/morocco-main.jpg" alt="picture of madrid" />
-                <p>Hello Morocco!</p>
-              </div>
-            </Link>
-            <Link>
-              <div>
-                <img src="./img/cathedral.png" alt="picture of madrid" />
-                <p>Bonjour Cote d´Azur </p>
-              </div>
-            </Link>
-            <Link>
-              <div>
-                <img src="./img/italy.png" alt="picture of madrid" />
-                <p>Buongiorno Italy</p>
-              </div>
-            </Link>
-          </menu>
+          <h3>About me</h3>
+          <p>
+            Hi. I am Aisha Kamilah; travel enthusiast and blogger. I am from the
+            beautiful twin isles of Trinidad and Tobago. Now I live in Spain and
+            travel frequently across Europe. For the past couple years and after
+            visiting over 30 cities my perspectives and visions of all that I
+            have known continues to be challenged. I want to somehow share my
+            stories with the world.
+          </p>
+          <p>
+            I am also a software developer. You can visit my programming blog
+            here.
+          </p>
         </div>
-      </div>
+      </section>
+      <section>
+        <h1 className={styles.recentposts}>Recent Posts</h1>
+        <div className={styles.posts}>
+          <FeatureImage
+            text="hello morocco"
+            image="./img/morocco-main.jpg"
+            onClick={() => history.push("/travel/morocco")}
+          ></FeatureImage>
+
+          <FeatureImage
+            text="Bonjour Cote d´Azur"
+            image="./img/cathedral.png"
+          ></FeatureImage>
+          <FeatureImage
+            text="Buongiorno Italy"
+            image="./img/italy.png"
+          ></FeatureImage>
+        </div>
+      </section>
     </main>
   );
 }
