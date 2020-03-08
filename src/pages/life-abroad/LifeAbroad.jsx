52510e30933bc3a8fc78773e1826b3b34d521ed5
Aisha Gregg <aishagregg@gmail.com>
2020-03-08 22:23:09 +0100

    Update life abroad section

--- src/pages/life-abroad/LifeAbroad.jsx
+++ src/pages/life-abroad/LifeAbroad.jsx
@@ -19,40 +19,60 @@ export function LifeAbroad() {
         <strong> 1.Food</strong>
         <p>
           One big difference is the local food here. It is fine but coming from
-          a pallete that is used to alot of flavour and spice it can be
+          a palate that is used to alot of flavour and spice it can be
           underwhelming at times.The dishes of my country usually have some
           indian influence and I found that the local plates like paella, fries
-          and egg and croquetas were not as amazing as many tourists
-          claimed.Although I did have the opportunity to have homemade paella
-          and it was amazing.A lot of the food here grew on me though
-          (eventually) and I enjoy it now thankfully.
+          and egg and croquetas were not as amazing as many tourists claimed.A
+          sunny side up runny egg is also a big thing here, which honestly I
+          still don´t get lol. I did eventually have the opportunity to have
+          homemade paella and it was amazing.A lot of the food here grew on me
+          though and I enjoy it now ...thankfully.
         </p>
         <strong>2.Weather</strong>
         <p>
           I do not like the cold. In fact, I am one they would call "friolera"
           here.( Sommeone who always feels cold) Of course, coming from the
-          caribbean people always ask me why I came to Europe. I sometimes
-          reflect on it, especially in the winter.But the truth is the winters
-          here are not THAT bad and we are not bombarded by snow.It is usually
-          moderate.The most troublesome thing is not having a wadrobe big enough
-          to have winter and summer clothes sections and having to retire your
-          summer clothes in a suitcase in order to revive your winter jackets,
-          scarfs and boots...Not that fun.I remember always having to check the
-          weather app before I left the house too.Feeling cold in your home and
-          not having sufficient heating to sleep.Also not fun lol The summers
-          here are hot and sticky.But I still prefer it over winter.I can wear
-          almost anything I want.Dont really need to depend on the weather app
-          too much and I am apparently so much lighter.But yes suncreen,
-          suncreen suncreen.Very important!
+          caribbean people always ask me why I came to Europe. What was I
+          thinking? I sometimes reflect on it, especially in the winter.But the
+          truth is the winters here are not THAT bad and we are not bombarded by
+          snow.It is usually moderate.The most troublesome thing is not having a
+          wadrobe big enough to have winter and summer clothes sections and
+          having to retire your summer clothes in a suitcase in order to revive
+          your winter jackets, scarfs and boots...Not that fun.I remember always
+          having to check the weather app before I left the house too.Feeling
+          cold in your home and not having sufficient heating to sleep.Also not
+          fun lol. The summers here are hot and sticky.But I still prefer it
+          over winter.I can wear almost anything I want.Dont really need to
+          depend on the weather app too much and I am apparently so much
+          lighter.But yes suncreen, suncreen suncreen.Very important!Because
+          that sun will burn you alive here and there are barely any clouds
+          during the hot summer months!
         </p>
         <strong>3.Culture</strong>{" "}
         <p>
           Everyone is always out even on weekdays.It is always a social scene, I
           would have to say even more than my own country and we do love a party
+          there.It is super commom to see a lot of people having drinks after
+          work ANY DAY of the week and yes that means even on Monday.I suppose
+          older people with kids not so much but you may even see a small child
+          at 10pm on a Wednesday in a restaurant type bar with their parents
+          while they have a drink and meal.
         </p>
-        <strong>4.Racism</strong> <p></p>
+        <strong>4.Racism</strong>{" "}
+        <p>
+          So this is a topic I have heard about in certain countries in Europe
+          and Spain is no exception.However, I would have to say I am lucky to
+          be living in a big city with many cultural influences and ethnicities
+          and I have not seen many cases of racism.I would share however, times
+          when I felt a little isolated and discriminated against, racism even;
+          for example a lady not sitting next to me on the underground ... but
+          that may be a topic for another blog or vlog.
+        </p>
         <strong>5.Language Barriers </strong>
-        <p></p>
+        <p>
+          It feels nice to feel like you fit in somewhere and
+          belong.However,some
+        </p>
         Some say curisosity kills the cat but what if curiosity makes you alive
         once again?
       </ul>
