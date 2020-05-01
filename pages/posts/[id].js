256f8e9726118fc978927e79c4b44190b5838670
Aisha Gregg <aishagregg@gmail.com>
2020-05-01 23:35:05 +0200

    Remove next folder

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import React from "react";
+import { getPostIds, getPost } from "../../posts-repository";
+import styles from "./post.module.css";
+import { DiscussionEmbed } from "disqus-react";
+import { Page } from "../../components/page/Page";
+
+export default function Post({ post }) {
+  const disqusConfig = {
+    url: "https://aishagregg.disqus.com/",
+    identifier: id,
+    title: id,
+  };
+  return (
+    <Page>
+      <div
+        className={styles.post}
+        dangerouslySetInnerHTML={{ __html: post }}
+      ></div>
+      <DiscussionEmbed config={disqusConfig} shortname="aishagregg" />
+    </Page>
+  );
+}
+
+export function getStaticPaths() {
+  const paths = getPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const post = await getPost(params.id);
+  return {
+    props: {
+      post,
+    },
+  };
+}
